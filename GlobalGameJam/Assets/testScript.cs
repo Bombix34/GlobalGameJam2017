56f851cc6d7d0766a42ad6584ccb61caf66e7ea4
Hugo Bombail <hugo.bombail@wanadoo.fr>
2017-01-21 01:06:04 +0100

    - premiere version des waves

---
+++ GlobalGameJam/Assets/testScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour {
+
+	public GameObject testWave;
+
+
+	void Start () {
+		
+	}
+	
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+
+			Instantiate(testWave, new Vector2(Random.Range(-5f,5f),Random.Range(-5f,5f)), Quaternion.identity);
+		}
+	}
+}
