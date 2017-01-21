07a44bcb223e90c5e3a16d4b79eadc910ae61a02
DESKTOP-FVU2GRD\Nairolf <florian.carnac@gmail.com>
2017-01-21 01:04:16 +0100

    Script joueur de base2

--- GlobalGameJam/Assets/BehaviourScript.cs
+++ GlobalGameJam/Assets/BehaviourScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class BehaviourScript : MonoBehaviour {
 
-    private float Speed = 0.5f;
+    private float Speed = 0.25f;
     private float _x = 0f;
     private float _y = 0f;
     private bool dashUp;
@@ -23,28 +23,16 @@ public class BehaviourScript : MonoBehaviour {
         }else {
             dashUp = true;
         }
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            _y = 1.0f;
-        }else if (Input.GetKey(KeyCode.DownArrow)) {
-            _y = -1.0f;
-        }else { 
-            _y = 0.0f;
-        }
-        if(Input.GetKey(KeyCode.LeftArrow)) {
-            _x = -1.0f;
-        }else if(Input.GetKey(KeyCode.RightArrow)) {
-            _x = 1.0f;
-        }else {
-            _x = 0.0f;
-        }
+        _x = Input.GetAxis("Horizontal");
+        _y = Input.GetAxis("Vertical");
+        
         if(Input.GetKey(KeyCode.LeftShift) && dashUp) {
-            _x += 5 * System.Math.Sign(_x);
-            _y += 5 * System.Math.Sign(_y);
+            _x *= 7;
+            _y *= 7;
             dashUp = false;
             dashCd = 1;
         }
         Vector2 move = new Vector2(_x, _y);
-        //transform.position += move * Speed * Time.deltaTime;
         rb2d.MovePosition(rb2d.position + move * Speed);
 	}
 }
